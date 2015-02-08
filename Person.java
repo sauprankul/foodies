1a0a77cc19b62604c9c64baf171ee2e77850cf25
rohan-varma <rvarm1@gmail.com>
2015-02-07 15:01:18 -1000

    Update Person.java

--- Person.java
+++ Person.java
@@ -7,19 +7,37 @@ import java.util.ArrayList;
  */
 public class Person {
 String name;
-ArrayList<Restaurant> favoriteRestaurants = new ArrayList<Restaurant>();
-
-    public void generateFavoriteRestaurants(){
-        RestaurantList mahList = new RestaurantList();
-        for(int i = 0; i <mahList.getList().size(); i++){
-            if(mahList.getList().get(i).getFavorite() == true){
-                favoriteRestaurants.add(mahList.getList().get(i));
-            }
-        }//end for
-    }//end method
-    public Person(String name){
-        this.name = name;
-        generateFavoriteRestaurants();
+ArrayList<Restaurant> favorites;
+ArrayList<Item> foods;
+    
+public Person(String name){
+    this.name = name;
+    favorites = null;
+    foods = null;
+}
+    
+   
+    
+public void addFavorites(Restaurant r){
+    favorites.add(r);
+}
+   
+    void addFoods(Item i){
+        foods.add(i);
     }
+    
+    
+    ArrayList<Restaurant> getFavoriteRestaurants(){
+        return favorites;
+    }
+    ArrayList<Item> getFavoriteFoods(){
+        return foods;
+    }
+    
+   String getName(){
+        return name;
+    }
+    
+    
 
 }
