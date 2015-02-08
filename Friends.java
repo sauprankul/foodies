0a955de76b5abd028d86828bf2a7917193c01206
rohan-varma <rvarm1@gmail.com>
2015-02-07 16:37:43 -1000

    Update Friends.java

--- Friends.java
+++ Friends.java
@@ -1,23 +1,45 @@
+package com.evhacks.qrmenu;
 import java.util.ArrayList;
+import java.util.Random;
 
 
 public class Friends {
-	RestaurantList myList = new RestaurantList();
-
+    RestaurantList myList = new RestaurantList();
+    ArrayList<Person> friends = new ArrayList<Person>();
     public Friends(){
         //creating James
-    	Person James = new Person("James");
-    	James.addFavorites(myList.getList().get(0));
-    	James.addFavorites(myList.getList().get(1));
-    	
-    	//creating Amy
-    	Person Amy = new Person("Amy");
-    	Amy.addFavorites(myList.getList().get(3));
-    	Amy.addFavorites(myList.getList().get(1));
-    
-    	//creating SpongeBob
-    	Person Spongebob = new Person("Spongebob");
-    	Spongebob.addFavorites(myList.getList().get(2));
-    	Spongebob.addFavorites(myList.getList().get(4));
+        Person James = new Person("James");
+        Random r = new Random(5);
+
+        for(int i = 0; i < 2; i++) {
+            Restaurant j = myList.getList().get(r.nextInt());
+            James.addFavorites(j);
+            int x = new Random(j.getMenu().size()).nextInt();
+            James.addFoods(j.getMenu().get(x), j);
+
+
+        }
+
+
+        //creating Amy
+        Person Amy = new Person("Amy");
+        for(int i = 0 ; i < 2; i++) {
+            Restaurant j = myList.getList().get(r.nextInt());
+            Amy.addFavorites(j);
+            int x = new Random(j.getMenu().size()).nextInt();
+            Amy.addFoods(j.getMenu().get(x), j);
+        }
+
+
+        //creating Jane
+        Person Jane = new Person("Jane");
+        for(int i = 0 ; i < 2 ; i ++){
+            Restaurant j = myList.getList().get(r.nextInt());
+            Jane.addFavorites(j);
+            int x = new Random(j.getMenu().size()).nextInt();
+            Jane.addFoods(j.getMenu().get(x), j);
+        }
+        friends.add(Jane); friends.add(Amy); friends.add(James);
+
     }
 }
