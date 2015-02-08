e64d8b6fce2c272133445b022bc2641e0bb70555
rohan-varma <rvarm1@gmail.com>
2015-02-08 03:03:52 -1000

    Update Menu.java

--- app/src/main/java/com/evhacks/foodies/Menu.java
+++ app/src/main/java/com/evhacks/foodies/Menu.java
@@ -1,9 +1,29 @@
 package com.evhacks.foodies;
 
+import java.util.ArrayList;
+
 /**
- * Created by skwis_000 on 2/7/2015.
+ * Created by rats on 2/7/2015.
  */
 public class Menu {
+
+    ArrayList<Item> getMenuItems(String restname){
+        ArrayList<Item> m = new ArrayList<Item>();
+        RestaurantList myList = new RestaurantList();
+        ArrayList<Restaurant> r = myList.getList();
+        for(int i = 0 ; i < r.size(); i++){
+            if(restname == r.get(i).getName()){
+
+                m =   r.get(i).getMenu();
+                return m;
+            }
+        }
+        return null;
+    }
+
+
+
+
 
 
 }
